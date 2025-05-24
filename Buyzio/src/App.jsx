6aa4ef3fc4239fee30eb58ef379a16e10c52df69
Aaman <heyaaman2004@gmail.com>
2025-05-25 04:03:29 +0530

    Add files via upload

---
+++ Buyzio/src/App.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Routes, Route, Navigate, useLocation } from 'react-router-dom';
+import { AnimatePresence } from 'framer-motion';
+
+import Signup from './components/Signup';
+import Login from './components/Login';
+import Home from './components/Home';
+import ThemeToggle from './components/ThemeToggle';
+import Profile from './components/Profile';
+import Goodbye from './components/Goodbye'; 
+
+const App = () => {
+  const location = useLocation(); 
+
+  return (
+    <>
+      <ThemeToggle />
+      <AnimatePresence mode="wait" initial={false}>
+        <Routes location={location} key={location.pathname}>
+          <Route path="/" element={<Home />} />
+          <Route path="/home" element={<Navigate to="/" />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/goodbye" element={<Goodbye />} /> 
+        </Routes>
+      </AnimatePresence>
+    </>
+  );
+};
+
+export default App;
