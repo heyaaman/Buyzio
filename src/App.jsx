d6ffe70f477b90b1fca6d28d0f5c80697ebca492
Aaman <heyaaman2004@gmail.com>
2025-06-05 20:41:13 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,12 @@ import Home from './components/Home';
 import ThemeToggle from './components/ThemeToggle';
 import Profile from './components/Profile';
 import Goodbye from './components/Goodbye'; 
+import Checkout from './components/Checkout';
+import Success from './components/Success';
+import Cancel from './components/Cancel';
+import Cart from './components/Cart';
+import AdminDashboard from './components/AdminDashboard';
+import AdminLogin from './components/AdminLogin';
 
 const App = () => {
   const location = useLocation(); 
@@ -23,7 +29,12 @@ const App = () => {
           <Route path="/signup" element={<Signup />} />
           <Route path="/profile" element={<Profile />} />
           <Route path="/goodbye" element={<Goodbye />} /> 
-          <Route path="/Cart" element={<Cart />} />
+          <Route path="/checkout" element={<Checkout/>} /> 
+          <Route path="/success" element={<Success/>} /> 
+          <Route path="/cancel" element={<Cancel/>} /> 
+          <Route path="/cart" element={<Cart/>} /> 
+          <Route path="/admin" element={<AdminDashboard />} />
+          <Route path="/admin-login" element={<AdminLogin />} />
         </Routes>
       </AnimatePresence>
     </>
